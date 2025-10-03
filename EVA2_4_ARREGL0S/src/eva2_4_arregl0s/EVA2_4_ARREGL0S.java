fdf5ac44d8f3c73541cdf0f1056ba0cfc4bc9309
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-10-02 22:53:59 -0700

    Add files via upload

---
+++ EVA2_4_ARREGL0S/src/eva2_4_arregl0s/EVA2_4_ARREGL0S.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_4_arregl0s;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA2_4_ARREGL0S {
+
+    /**
+     * @param args the command line arguments
+     */ 
+    public static void main(String[] args) {
+        // Un arreglo guarda una cantidad grande de datos
+        int[] arreglo = new int[5]; // crea un arreglo de 5 enteros
+
+        arreglo[0] = 100;
+        arreglo[1] = 200;
+        arreglo[2] = 300;
+        arreglo[3] = 400;
+        arreglo[4] = 500;
+
+        // Para leer el arreglo
+        System.out.println("Arreglo [0] = " + arreglo[0]);
+        System.out.println("Arreglo [1] = " + arreglo[1]);
+        System.out.println("Arreglo [2] = " + arreglo[2]);
+        System.out.println("Arreglo [3] = " + arreglo[3]);
+        System.out.println("Arreglo [4] = " + arreglo[4]);
+    }
+}
