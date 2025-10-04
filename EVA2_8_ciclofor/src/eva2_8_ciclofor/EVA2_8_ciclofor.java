5d685a51248f42d0905ccad6dadc46a2995a75ee
chanclazoabidal-afk <chanclazoabidal@gmail.com>
2025-10-03 20:51:41 -0700

    Add files via upload

---
+++ EVA2_8_ciclofor/src/eva2_8_ciclofor/EVA2_8_ciclofor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_8_ciclofor;
+
+/**
+ *
+ * @author Erick
+ */
+public class EVA2_8_ciclofor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //3 parametros
+        //variable y valor de inicio
+        //hasta donde llegas condicion --> debe de ser verdad
+        // cuando es falso el for se detiene
+        //incremento o decremento
+        //sumar o restar
+        //operadores; agregar o restar 1
+        //++ --
+       
+        
+        for(int i=1; i<=10; i++){
+            System.out.println("hola mundo");
+        }
+    }
+    
+}
